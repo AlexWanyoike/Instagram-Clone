7c976661e76de8afb92907f6628255fcbd5305ad
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-05-22 22:59:54 +0300

    Migration installed and Initialized

--- mellanex/settings.py
+++ mellanex/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'mellanex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mellanex',
+        'USER': 'alex',
+    'PASSWORD':'roverson3',
     }
 }
 
@@ -111,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
